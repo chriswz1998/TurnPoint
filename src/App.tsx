4b7dc963480ab14309a8624e4c882d28fdcf90ba
chris.wz <scissorwz@gmail.com>
2025-02-26 02:20:37 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UploadFile from "@/components/upload-file.tsx";
+
+function App() {
+  return (
+      <div className="w-full h-[100vh] flex flex-col items-center justify-center">
+          <UploadFile/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/lib/mock.ts
@@ -0,0 +1,46 @@
+export const filetypes = [
+    {
+        name: 'Intake Reporting',
+        id: '1',
+    },
+    {
+        name: 'Flow Through',
+        id: '2',
+    },
+    {
+        name: 'Loss of Service',
+        id: '3',
+    },
+    {
+        name: 'Rent Supplement Request',
+        id: '4',
+    },
+    {
+        name: 'Goals and Progress',
+        id: '5',
+    },
+    {
+        name: 'Safety Plan',
+        id: '6',
+    },
+    {
+        name: 'Overdose Safety Plan',
+        id: '7',
+    },
+    {
+        name: 'Incident Report',
+        id: '8',
+    },
+    {
+        name: 'Individuals',
+        id: '9',
+    },
+    {
+        name: 'Shelter Diversion Follow-Up Log',
+        id: '10',
+    },
+    {
+        name: 'Site List',
+        id: '11',
+    }
+]
